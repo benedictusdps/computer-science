285ff8cdd93e8ea74e6a73258c002f51830714ca
benedictusdps <benedictusdps@gmail.com>
2022-04-02 21:48:58 +0700

    Add files from upload

---
+++ CS101_Introduction_to_Programming/01_initials.py
@@ -0,0 +1,13 @@
+# ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.
+# Write a Python program called initials.py that displays the initials of your name in block letters as shown and dip your toes into ASCII art.
+
+# My name is Benedictus See
+# I like cooking
+
+print(" BBBB    SSS  ")
+print(" B   B  S   S ")
+print(" B   B  S     ")
+print(" BBBB    SSS  ")
+print(" B   B      S ")
+print(" B   B  S   S ")
+print(" BBBB    SSS  ")
---
+++ CS101_Introduction_to_Programming/03_magic8.py
@@ -0,0 +1,44 @@
+# The Magic 8-Ball is a popular toy developed in the 1950s for fortune-telling or advice seeking.
+
+# rite a magic8.py Python program that can answer any “Yes” or “No” question with a different fortune each time it executes.
+
+import random
+
+name = "Beno"
+question = "Am I handsome?"
+answer = " "
+
+random_number = random.randint(1, 10)
+
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+elif random_number == 10:
+  answer = "No, just no."
+else:
+  answer = "Error"
+
+# Output the asker's name and their question
+if name == "":
+  print("Question:", question)
+else:
+  print(name, "asks:", question)
+if question == "":
+  print("Please insert a question.")
+else:
+  print("Magic 8-Ball's answer:", answer)
